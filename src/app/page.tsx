e00242cf3276ccfe12a0579c51ee8d3471afbdc2
Nkachukwu Victor <nkachukwuvictor2016@gmail.com>
2024-05-09 00:10:25 +0100

    newly updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,17 @@ import { InteractiveForm } from "@/components/interactive-form";
 import ClientOnly from "@/components/client-only";
 import SlideButtons from "@/components/interactive-form/slide-buttons";
 import { HeroHighlight } from "@/components/interactive-form/hero-highlights";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
     <HeroHighlight>
-      <section className="w-full h-screen h-[calc(100dvh)] overflow-auto sm:overflow-hidden relative flex flex-col">
+      <section
+        className={cn(
+          "w-full h-screen overflow-auto sm:overflow-hidden relative flex flex-col",
+          "h-[calc(100dvh)]"
+        )}
+      >
         <ClientOnly>
           <InteractiveForm />
           <div className="w-full flex items-center justify-end static sm:absolute bottom-0 right-0  p-4">
