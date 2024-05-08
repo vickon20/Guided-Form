dccfa832c391d6bbb688bda9cd4b80c045d8cde3
Nkachukwu Victor <nkachukwuvictor2016@gmail.com>
2024-05-09 00:06:53 +0100

    Updated final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { HeroHighlight } from "@/components/interactive-form/hero-highlights";
 export default function Home() {
   return (
     <HeroHighlight>
-      <section className="w-full h-[calc(100dvh)] overflow-auto sm:overflow-hidden relative flex flex-col">
+      <section className="w-full h-screen h-[calc(100dvh)] overflow-auto sm:overflow-hidden relative flex flex-col">
         <ClientOnly>
           <InteractiveForm />
           <div className="w-full flex items-center justify-end static sm:absolute bottom-0 right-0  p-4">
