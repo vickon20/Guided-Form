d5b77799bcdc3ae9ed337e7474ee01df8224f075
Nkachukwu Victor <nkachukwuvictor2016@gmail.com>
2024-07-05 19:44:17 +0100

    update mentor routes

--- src/app/api/mentors/route.ts
+++ src/app/api/mentors/route.ts
@@ -5,28 +5,51 @@ export async function GET(request: Request) {
 
   const id = searchParams.get("id");
   const email = searchParams.get("email");
+  const secret = searchParams.get("secret");
 
-  if (!!id) {
-    const mentors = await db.guidedForm.findUnique({ where: { id } });
-    return Response.json({
-      status: "success",
-      data: mentors,
-      message: "mentors retrieved successfully",
-    });
-  } else if (!id && !!email) {
-    const mentors = await db.guidedForm.findFirst({ where: { email } });
-    return Response.json({
-      status: "success",
-      data: mentors,
-      message: "mentors retrieved successfully",
-    });
-  } else {
-    const mentors = await db.guidedForm.findMany();
-    return Response.json({
-      status: "success",
-      message: "all mentors received successfully",
-      data: mentors,
-    });
+  if (!secret || secret !== process.env.APP_SECRET) {
+    return Response.json(
+      {
+        status: "failure",
+        message: "You are not authorized to make this request",
+        data: null,
+      },
+      { status: 403 }
+    );
+  }
+
+  try {
+    if (!!id) {
+      const mentors = await db.guidedForm.findUnique({ where: { id } });
+      return Response.json({
+        status: "success",
+        data: mentors,
+        message: "mentors retrieved successfully",
+      });
+    } else if (!id && !!email) {
+      const mentors = await db.guidedForm.findFirst({ where: { email } });
+      return Response.json({
+        status: "success",
+        data: mentors,
+        message: "mentors retrieved successfully",
+      });
+    } else {
+      const mentors = await db.guidedForm.findMany();
+      return Response.json({
+        status: "success",
+        message: "all mentors received successfully",
+        data: mentors,
+      });
+    }
+  } catch (error) {
+    return Response.json(
+      {
+        status: "failure",
+        message: "Internal server error",
+        data: null,
+      },
+      { status: 500 }
+    );
   }
 }
 
